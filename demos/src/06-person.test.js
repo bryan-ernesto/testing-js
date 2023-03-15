e4f9f1607f83a3712307e5f36d8151ec5ac54aed
Bryan Gamez <bgamez@groupnova.com.gt>
2023-03-15 11:39:58 -0600

    AAA

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,12 +2,16 @@ const Person = require('./06-person');
 
 describe('Test for person', () => {
   let person;
+  // Arrange / Given
   beforeEach(() => {
     person = new Person('Bryan', 45, 1.7);
   });
   test('should return down', () => {
+    // Arrange / Given
     person.weight = 45;
+    // Act / When
     const imc = person.calcIMC();
+    // Assert / Then
     expect(imc).toBe('down');
   });
 
